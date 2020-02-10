484c9502bac76501fd0d593db644c92ee17421aa
Kamil Jarmolowicz <kamiljarmol@protonmail.com>
2020-02-10 15:25:17 +0100

    removed Options.js (redundant) - same thing achieved with NavBar.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
 
 import Header from "./layout/Header";
@@ -8,20 +9,14 @@ import Footer from "./layout/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header>
-        <Header />
-      </header>
-      <nav>
-        <NavBar />
-      </nav>
-      <main>
-        <Matrix />
-      </main>
-      <footer>
-        <Footer />
-      </footer>
-    </div>
+    <Router>
+      <div className="App">
+        <header>{<Header />}</header>
+        <nav>{<NavBar />}</nav>
+        <main>{<Matrix />}</main>
+        <footer>{<Footer />}</footer>
+      </div>
+    </Router>
   );
 }
 
--- src/layout/NavBar.js
+++ src/layout/NavBar.js
@@ -1,13 +1,13 @@
-import React, { Component } from "react";
+import React from "react";
 import "../style/NavBar.css";
-import Options from "../elements/Options";
-import { BrowserRouter } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const options = [
   {
     id: 1,
     name: "Main site",
-    redirect: "/"
+    redirect: "/",
+    exact: true
   },
   {
     id: 2,
@@ -21,15 +21,20 @@ const options = [
   }
 ];
 
-class NavBar extends Component {
-  render() {
-    return (
-      <BrowserRouter>
-        <ul className="nav">
-          <Options options={options} />
-        </ul>
-      </BrowserRouter>
-    );
-  }
-}
+const NavBar = () => {
+  const menu = options.map(option => (
+    <em>
+      <li class="option" key={option.id}>
+        <NavLink
+          to={option.redirect}
+          exact={option.exact ? option.exact : false}
+        >
+          {option.name}
+        </NavLink>
+      </li>
+    </em>
+  ));
+  return <ul class="nav">{menu}</ul>;
+};
+
 export default NavBar;
