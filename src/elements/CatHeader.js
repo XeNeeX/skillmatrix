a1b447eeb476d784d0c756591d3c6e47fd391a6f
Kamil Jarmolowicz <kamiljarmol@protonmail.com>
2020-02-18 14:52:59 +0100

    recommit

--- src/elements/CatHeader.js
+++ src/elements/CatHeader.js
@@ -1,11 +1,5 @@
 import React from 'react';
 
-const names = [
-    { id: "1", name: "HSE", class: "hse" },
-    { id: "2", name: "Finance", class: "fin" },
-    { id: "3", name: "Logistics", class: "log" },
-    { id: "4", name: "Management", class: "mgm" },
-]
 
 const CatHeader = () => {
     return (
